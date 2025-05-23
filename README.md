25831f7de55c8a53534f933c30d53557f70728fd
razyousuf <23010492@hope.ac.uk>
2025-05-23 01:49:25 +0100

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # NLP-Text-Classification-Pipeline
+
+## Running the programme
+
+```bash
+conda create -n nlp python=3.10 -y
+```
+
+```bash
+conda activate nlp
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="speech-classifier",
+    version="0.0.1",
+    author="Saurabh",
+    author_email="razyousufi350@gmail.com",
+    description="A hate speech classification project",
+    packages=find_packages(),
+    install_requires=[],
+)
